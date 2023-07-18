83051322e682dc09754a7898ddcdfd64820924f2
RuhulAmin3 <dev.ruhulamin3@gmail.com>
2023-07-18 15:24:43 +0600

    signup data catched

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,15 @@ import router from './routes/index.tsx';
 import {RouterProvider} from "react-router-dom";
 import { store } from './redux/store.ts';
 import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
       <RouterProvider router={router}/>
       <Provider store={store}>
       <App />
+      <Toaster/>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>,
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
-  Card,
   CardHeader,
   CardBody,
   CardFooter,
@@ -9,13 +11,34 @@ import {
 } from "@material-tailwind/react";
 import HeaderNavbar from "../components/HeaderNavbar";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 export default function SignupPage() {
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const handleChange = (field, value) => {
+    setUserData({ ...userData, [field]: value });
+  };
+  const handleSubmit = (e) => {
+    const { password, confirmPassword } = userData;
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      return toast.error("password doesn't match");
+    }
+    console.log(userData);
+  };
+
   return (
     <>
       <HeaderNavbar />
       <div className="flex items-center justify-center h-screen overflow-y-hidden">
-        <Card className="w-96">
+        <form className="w-96 shadow-lg" onSubmit={handleSubmit}>
           <CardHeader
             variant="gradient"
             color="green"
@@ -26,13 +49,45 @@ export default function SignupPage() {
             </Typography>
           </CardHeader>
           <CardBody className="flex flex-col gap-4">
-            <Input color="green" label="First Name" size="lg" />
-            <Input color="green" label="Last Name" size="lg" />
-            <Input color="green" label="Email" size="lg" />
-            <Input color="green" label="Password" size="lg" />
+            <Input
+              required
+              color="green"
+              label="First Name"
+              size="lg"
+              onChange={(e) => handleChange("firstName", e.target.value)}
+            />
+            <Input
+              required
+              color="green"
+              label="Last Name"
+              size="lg"
+              onChange={(e) => handleChange("lastName", e.target.value)}
+            />
+            <Input
+              required
+              color="green"
+              label="Email"
+              size="lg"
+              type="email"
+              onChange={(e) => handleChange("email", e.target.value)}
+            />
+            <Input
+              required
+              color="green"
+              label="Password"
+              size="lg"
+              onChange={(e) => handleChange("password", e.target.value)}
+            />
+            <Input
+              required
+              color="green"
+              label="Password"
+              size="lg"
+              onChange={(e) => handleChange("confirmPassword", e.target.value)}
+            />
           </CardBody>
           <CardFooter className="pt-0">
-            <Button color="green" variant="gradient" fullWidth>
+            <Button color="green" variant="gradient" type="submit" fullWidth>
               Create Account
             </Button>
             <Typography variant="small" className="mt-6 flex justify-center">
@@ -48,7 +103,7 @@ export default function SignupPage() {
               </Link>
             </Typography>
           </CardFooter>
-        </Card>
+        </form>
       </div>
     </>
   );
